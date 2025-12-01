4d9b06164550b4a51c0795b5b4877f0719b3bb37
Janusz Głowacki <jglowacki@int.pl>
2025-12-01 21:50:26 +0100

    uzytkownik1 linia 3
    Połączono

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 print('Main python program')
-print('Witaj świecie mój i hello world')
-print('Użytkownik 2 linia 3')
+print('Witaj świecie mój')
+print('Uzytkownik 1 linia 3 i Użytkownik 2 linia 3')
